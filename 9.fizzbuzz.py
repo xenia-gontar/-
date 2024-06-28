9b507cce9274ecb01b41a87dbe3cf398b82ff754
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-06-28 11:59:02 +0300

    Create 9.fizzbuzz.py

---
+++ 9.fizzbuzz.py
@@ -0,0 +1,11 @@
+while True:
+    number = int(input("Введите число: \n"))
+    for i in range(1, number):
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
